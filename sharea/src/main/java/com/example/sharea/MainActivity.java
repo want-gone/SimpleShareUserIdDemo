446f4206256ef734fe8af96384cdad7675d078f3
WangGang <wanggang@zoncare.cn>
2016-09-27 09:59:07 +0800

    第一次提交

---
+++ sharea/src/main/java/com/example/sharea/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.sharea;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+//    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        textView = (TextView) findViewById(R.id.textView1);
+        WriteSettings(this, "123");
+    }
+
+
+    public void WriteSettings(Context context, String data) {
+        FileOutputStream fOut = null;
+        OutputStreamWriter osw = null;
+        try {
+            //默认建立在data/data/xxx/file/
+            fOut = openFileOutput("settings.txt", MODE_PRIVATE);
+            osw = new OutputStreamWriter(fOut);
+            osw.write(data);
+            osw.flush();
+            Toast.makeText(context, "Settings saved", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(context, "Settings not saved", Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                osw.close();
+                fOut.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ shareb/src/main/java/com/example/shareb/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.shareb;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) this.findViewById(R.id.textView1);
+
+        try {
+            //获取程序A的context
+            Context ctx = this.createPackageContext("com.example.sharea", Context.CONTEXT_IGNORE_SECURITY);
+            String msg = ReadSettings(ctx);
+            Toast.makeText(this, "DealFile2 Settings read" + msg,
+                    Toast.LENGTH_SHORT).show();
+            WriteSettings(ctx, "deal file2 write");
+        } catch (PackageManager.NameNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+    public String ReadSettings(Context context) {
+        FileInputStream fIn = null;
+        InputStreamReader isr = null;
+        char[] inputBuffer = new char[255];
+        String data = null;
+        try {
+            //此处调用并没有区别，但context此时是从程序A里面获取的
+            fIn = context.openFileInput("settings.txt");
+            isr = new InputStreamReader(fIn);
+            isr.read(inputBuffer);
+            data = new String(inputBuffer);
+            textView.setText(data);
+            Toast.makeText(context, "Settings read", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(context, "Settings not read", Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                isr.close();
+                fIn.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return data;
+    }
+
+    public void WriteSettings(Context context, String data) {
+        FileOutputStream fOut = null;
+        OutputStreamWriter osw = null;
+        try {
+            fOut = context.openFileOutput("settings.txt", MODE_PRIVATE);
+            //此处调用并没有区别，但context此时是从程序A里面获取的
+            osw = new OutputStreamWriter(fOut);
+            osw.write(data);
+            osw.flush();
+            Toast.makeText(context, "Settings saved", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(context, "Settings not saved", Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                osw.close();
+                fOut.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
